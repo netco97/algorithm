cda62b62b25dacf3e2a24102967af80b83ee3003
isangmin <isangmin@localhost>
2022-03-24 19:22:50 +0900

    This is Commit Message!

---
+++ src/test_1/Solution.java
@@ -0,0 +1,36 @@
+package test_1;
+
+import java.util.Scanner;
+
+public class Solution
+{
+	Scanner Sc = new Scanner(System.in);
+	public Solution()
+	{
+		int A = Sc.nextInt();
+		int B = Sc.nextInt();
+		
+		int N = eucd(Math.max(A, B),Math.min(A, B));
+		
+		A=A/N; //최대공약수로 두수를 나눠서 몫구하는과정
+		B=B/N;
+		
+		int M = A*B*N; //최소공배수는 두수의 최대공약수 * 위에서 구한 
+		
+		System.out.println(N);
+		System.out.println(M);
+	}
+	
+	public int eucd(int a,int b)
+	{
+		int r = a%b;
+		if(r==0)
+		{
+			return b; 
+		}
+		else
+		{
+			return eucd(b,r);
+		}
+	}
+}
